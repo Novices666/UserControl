93ef14d65a194c890325aa18cfec1fb15c84fff6
novices666 <1601413678@qq.com>
2023-11-09 20:07:45 +0800

    Impl Java and Web all features
    Impl all features and add logically delete

--- UserControl/src/components/Footer/index.tsx
+++ UserControl/src/components/Footer/index.tsx
@@ -1,28 +1,28 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'CopyRight®Novices';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'NovicesShop',
+          title: 'Novices商城',
+          href: 'https://novices.cc/',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/novices666',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'NovicesMovie',
+          title: 'Novices影视',
+          href: 'https://www.novices.cc/',
           blankTarget: true,
         },
       ]}
